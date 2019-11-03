91063f17b89c7a3f5fd444337703f413aff2887c
Nikita <43641576+nik787@users.noreply.github.com>
2019-11-03 15:56:06 +0300

    ajax формы

--- js/main.js
+++ js/main.js
@@ -1,7 +1,71 @@
-var navigationBurger = document.querySelector('.navigation__burger');
-var navigationList = document.querySelector('.navigation__list');
-navigationBurger.addEventListener('click', openNavigationList);
+window.addEventListener("DOMContentLoaded", function() {
+  var isStorageSupport = true;
+  var loginStorage = "";
+  var emailStorage = "";
 
+  try {
+    loginStorage = localStorage.getItem("login");
+    emailStorage = localStorage.getItem("email");
+  } catch (error) {
+    isStorageSupport = false;
+  }
+
+  var navigationBurger = document.querySelector('.navigation__burger');
+  var navigationList = document.querySelector('.navigation__list');
+  navigationBurger.addEventListener('click', openNavigationList);
+
+  var form = document.querySelector(".contact__form");
+  var formLogin = form.querySelector("#name");
+  var formEmail = form.querySelector("#email");
+  var formInputs = form.querySelectorAll("input");
+  var button = form.querySelector(".form__button");
+  var status = document.querySelector(".form__status");
+  formLogin.value = loginStorage;
+  formEmail.value = emailStorage;
+
+  function success() {
+    form.reset();
+    formLogin.value = loginStorage;
+    formEmail.value = emailStorage;
+    status.innerHTML = "Спасибо!";
+
+  }
+
+  function error() {
+    status.innerHTML = "Ууупс! Что-то пошло не так.";
+  }
+
+  form.addEventListener("submit", function(ev) {
+    ev.preventDefault();
+    for (var i = 0; i < formInputs.length; i++) {
+      formInputs[i].setAttribute("required", true);
+      if(!formInputs[i].validity.valid) {
+        ev.preventDefault();
+      } else if(isStorageSupport) {
+        localStorage.setItem("name", formLogin.value);
+        localStorage.setItem("email", formEmail.value);
+      }
+    }
+
+    var data = new FormData(form);
+    ajax(form.method, form.action, data, success, error);
+  });
+});
+
+function ajax(method, url, data, success, error) {
+  var xhr = new XMLHttpRequest();
+  xhr.open(method, url);
+  xhr.setRequestHeader("Accept", "application/json");
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState !== XMLHttpRequest.DONE) return;
+    if (xhr.status === 200) {
+      success(xhr.response, xhr.responseType);
+    } else {
+      error(xhr.status, xhr.response, xhr.responseType);
+    }
+  };
+  xhr.send(data);
+}
 function openNavigationList(e) {
   e.preventDefault();
   navigationList.classList.toggle('navigation__list--animatedIn');
