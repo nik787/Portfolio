3da81506b5461a0128d6bf8ac94bfebab294f586
Nikita <43641576+nik787@users.noreply.github.com>
2020-01-13 14:46:13 +0300

    поправил js

--- js/main.js
+++ js/main.js
@@ -2,14 +2,6 @@ window.addEventListener("DOMContentLoaded", function() {
   var isStorageSupport = true;
   var loginStorage = "";
   var emailStorage = "";
-
-  try {
-    loginStorage = localStorage.getItem("login");
-    emailStorage = localStorage.getItem("email");
-  } catch (error) {
-    isStorageSupport = false;
-  }
-
   var navigationBurger = document.querySelector('.navigation__burger');
   var navigationList = document.querySelector('.navigation__list');
   navigationBurger.addEventListener('click', openNavigationList);
@@ -23,6 +15,15 @@ window.addEventListener("DOMContentLoaded", function() {
   formLogin.value = loginStorage;
   formEmail.value = emailStorage;
 
+  try {
+    loginStorage = localStorage.getItem("login");
+    emailStorage = localStorage.getItem("email");
+  } catch (error) {
+    isStorageSupport = false;
+  }
+
+  
+
   function success() {
     form.reset();
     formLogin.value = loginStorage;
@@ -33,6 +34,33 @@ window.addEventListener("DOMContentLoaded", function() {
 
   function error() {
     status.innerHTML = "Ууупс! Что-то пошло не так.";
+  }
+
+  function ajax(method, url, data, success, error) {
+    var xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState !== XMLHttpRequest.DONE) return;
+      if (xhr.status === 200) {
+        success(xhr.response, xhr.responseType);
+      } else {
+        error(xhr.status, xhr.response, xhr.responseType);
+      }
+    };
+    xhr.send(data);
+  }
+  function openNavigationList(e) {
+    e.preventDefault();
+    navigationList.classList.toggle('navigation__list--animatedIn');
+    setTimeout(() => {
+      navigationList.classList.toggle('navigation__list--show');
+    }, 100);
+    window.addEventListener("keydown", function(e) {
+      if (e.keyCode === 27) {
+        openNavigationList(e);
+      }
+    });
   }
 
   form.addEventListener("submit", function(ev) {
@@ -52,29 +80,4 @@ window.addEventListener("DOMContentLoaded", function() {
   });
 });
 
-function ajax(method, url, data, success, error) {
-  var xhr = new XMLHttpRequest();
-  xhr.open(method, url);
-  xhr.setRequestHeader("Accept", "application/json");
-  xhr.onreadystatechange = function() {
-    if (xhr.readyState !== XMLHttpRequest.DONE) return;
-    if (xhr.status === 200) {
-      success(xhr.response, xhr.responseType);
-    } else {
-      error(xhr.status, xhr.response, xhr.responseType);
-    }
-  };
-  xhr.send(data);
-}
-function openNavigationList(e) {
-  e.preventDefault();
-  navigationList.classList.toggle('navigation__list--animatedIn');
-  setTimeout(() => {
-    navigationList.classList.toggle('navigation__list--show');
-  }, 100);
-  window.addEventListener("keydown", function(e) {
-    if (e.keyCode === 27) {
-      openNavigationList(e);
-    }
-  });
-}
+
